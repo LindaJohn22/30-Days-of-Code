2432dfa166389b0ce8f52e0f83dca371f82833f6
LindaJohn22 <54841627+LindaJohn22@users.noreply.github.com>
2020-03-20 21:17:17 +0530

    Update day6_String&Loops.java

--- day6_String&Loops.java
+++ day6_String&Loops.java
@@ -1,6 +1,6 @@
-//Problem Statement: Given a string,S, of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 2 space-separated strings on a single line.
+/* //Problem Statement: Given a string,S, of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 2 space-separated strings on a single line.
 //Language Used: Java
-//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------ */
 
 import java.io.*;
 import java.util.*;
