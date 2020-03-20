458c069ec350cc0b4a79e201c4fea7b339b20b79
LindaJohn22 <54841627+LindaJohn22@users.noreply.github.com>
2020-03-20 21:14:26 +0530

    Update day0_Print.java

--- day0_Print.java
+++ day0_Print.java
@@ -1,6 +1,6 @@
-//Problem Statement:you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.
+/* //Problem Statement:you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.
 //Language Used: Java
-//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------ */
 
 public class Solution {
 	public static void main(String[] args) {
