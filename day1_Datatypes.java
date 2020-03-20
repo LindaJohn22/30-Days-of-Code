104e584ab967153c66b59494741529185476eb1e
LindaJohn22 <54841627+LindaJohn22@users.noreply.github.com>
2020-03-20 21:14:55 +0530

    Update day1_Datatypes.java

--- day1_Datatypes.java
+++ day1_Datatypes.java
@@ -1,4 +1,4 @@
-//Problem Statement: The variables i,d and s are already declared and initialized for you. You must:
+/* //Problem Statement: The variables i,d and s are already declared and initialized for you. You must:
 1.Declare 3 variables: one of type int, one of type double, and one of type String.
 2.Read 3 lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
 3.Use the + operator to perform the following operations:
@@ -6,7 +6,7 @@
 	2.Print the sum of d plus your double variable to a scale of one decimal place on a new line.
 	3.Concatenate s with the string you read as input and print the result on a new line.
 //Language Used: Java
-//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------ */
 
 import java.io.*;
 import java.util.*;
